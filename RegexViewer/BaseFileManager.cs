4282fae30bb13c8fe27a39a131bd188d5c3f68ca
jason gilbertson <jason.a.gilbertson@gmail.com>
2014-09-28 16:34:55 -0400

    basefilemanager add

---
+++ RegexViewer/BaseFileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Controls;
+
+namespace RegexViewer
+{
+    public abstract class BaseFileManager<T> : RegexViewer.IFileManager<T>
+    {
+        #region Private Fields
+
+        private static TraceSource _ts = new TraceSource("RegexViewer.LogManager");
+        private RegexViewerSettings settings = RegexViewerSettings.Settings;
+
+        #endregion Private Fields
+        public  static TraceSource ts = new TraceSource("RegexViewer.LogManager");
+        public RegexViewerSettings Settings = RegexViewerSettings.Settings;
+
+        #region Public Constructors
+
+        public BaseFileManager()
+        {
+            
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public List<IFileProperties<T>> Files { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public abstract bool CloseLog(string FileName);
+
+        public abstract IFileProperties<T> OpenFile(string LogName);
+
+
+        public abstract List<IFileProperties<T>> OpenFiles(string[] files);
+        
+
+        #endregion Public Methods
+    }
+}
